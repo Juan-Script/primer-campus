043ceee16bfc45ebca4af8331089ed352f04969e
Juarrior <juanreig197@gmail.com>
2023-05-19 12:14:12 +0200

    Registro creado correctamente

--- src/components/Forms/Formulario.jsx
+++ src/components/Forms/Formulario.jsx
@@ -1,5 +1,5 @@
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import React from "react";
+import { Formik, Field, ErrorMessage } from "formik";
+import React, { useState } from "react";
 import {
   useDisclosure,
   Button,
@@ -18,18 +18,31 @@ import {
 import * as Yup from "yup";
 
 const FormularioValues = {
-  fullname: "",
+  name: "",
   lastname: "",
   username: "",
   email: "",
   password: "",
 };
 
+const schema = Yup.object().shape({
+  name: Yup.string().required("Nombre es un campo requerido"),
+  lastname: Yup.string().required("Apellido es un campo requerido"),
+  username: Yup.string().required("Nombre de usuario es un campo requerido"),
+  email: Yup.string()
+    .required("Email es un campo requerido")
+    .email("Formato de email inválido"),
+  password: Yup.string()
+    .required("Contraseña es un campo requerido")
+    .min(8, "La contraseña debe tener al menos 8 caracteres"),
+});
+
 export function Formulario() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
+  const formRef = React.useRef(null);
 
   return (
     <>
@@ -44,53 +57,159 @@ export function Formulario() {
         Registrarse
       </Button>
 
-      <Formik>
-        <Modal
-          initialFocusRef={initialRef}
-          finalFocusRef={finalRef}
-          isOpen={isOpen}
-          onClose={onClose}
-        >
-          <ModalOverlay />
+      <Formik
+        validationSchema={schema}
+        initialValues={FormularioValues}
+        onSubmit={(values) => {
+          // console.log(values);
+          alert(JSON.stringify(values));
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => (
+          <Modal
+            initialFocusRef={initialRef}
+            finalFocusRef={finalRef}
+            isOpen={isOpen}
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <form noValidate onSubmit={handleSubmit}>
+              <ModalContent>
+                <ModalHeader>Crea tu cuenta</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody pb={6}>
+                  <FormControl>
+                    <FormLabel>Nombre</FormLabel>
+                    <Input
+                      ref={initialRef}
+                      placeholder="Nombre"
+                      name="name"
+                      value={values.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      borderColor={
+                        errors.name && touched.name ? "red" : undefined
+                      }
+                    />{" "}
+                    {errors.name && touched.name && (
+                      <ErrorMessage
+                        name="name"
+                        component="div"
+                        className="error"
+                      />
+                    )}
+                  </FormControl>
 
-          <ModalContent>
-            <ModalHeader>Crea tu cuenta</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody pb={6}>
-              <FormControl>
-                <FormLabel>Nombre</FormLabel>
-                <Input ref={initialRef} placeholder="Nombre" />
-              </FormControl>
+                  <FormControl mt={4}>
+                    <FormLabel>Apellido</FormLabel>
+                    <Input
+                      placeholder="Apellido"
+                      name="lastname"
+                      value={values.lastname}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      borderColor={
+                        errors.lastname && touched.lastname ? "red" : undefined
+                      }
+                    />
+                    {errors.lastname && touched.lastname && (
+                      <ErrorMessage
+                        name="lastname"
+                        component="div"
+                        className="error"
+                      />
+                    )}
+                  </FormControl>
 
-              <FormControl mt={4}>
-                <FormLabel>Apellido</FormLabel>
-                <Input placeholder="Apellido" />
-              </FormControl>
+                  <FormControl mt={4}>
+                    <FormLabel>Nombre de usuario</FormLabel>
+                    <Input
+                      placeholder="Nombre de usuario"
+                      name="username"
+                      value={values.username}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      borderColor={
+                        errors.username && touched.username ? "red" : undefined
+                      }
+                    />
+                    {errors.username && touched.username && (
+                      <ErrorMessage
+                        name="username"
+                        component="div"
+                        className="error"
+                      />
+                    )}
+                  </FormControl>
 
-              <FormControl mt={4}>
-                <FormLabel>Nombre de usuario</FormLabel>
-                <Input placeholder="Nombre de usuario" />
-              </FormControl>
+                  <FormControl mt={4}>
+                    <FormLabel>Email</FormLabel>
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      borderColor={
+                        errors.email && touched.email ? "red" : undefined
+                      }
+                    />
+                    {errors.email && touched.email && (
+                      <ErrorMessage
+                        name="email"
+                        component="div"
+                        className="error"
+                      />
+                    )}
+                  </FormControl>
 
-              <FormControl mt={4}>
-                <FormLabel type="email">Email</FormLabel>
-                <Input placeholder="Email" />
-              </FormControl>
+                  <FormControl mt={4}>
+                    <FormLabel>Contraseña</FormLabel>
+                    <Input
+                      type="password"
+                      placeholder="Contraseña"
+                      name="password"
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      borderColor={
+                        errors.password && touched.password ? "red" : undefined
+                      }
+                    />
+                    {errors.password && touched.password && (
+                      <ErrorMessage
+                        name="password"
+                        component="div"
+                        className="error"
+                      />
+                    )}
+                  </FormControl>
+                </ModalBody>
 
-              <FormControl mt={4}>
-                <FormLabel type="password">Contraseña</FormLabel>
-                <Input placeholder="Contraseña" />
-              </FormControl>
-            </ModalBody>
-
-            <ModalFooter>
-              <Button bg="#32d4a4" color="white" mr={3}>
-                Continuar
-              </Button>
-              <Button onClick={onClose}>Cancelar</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+                <ModalFooter>
+                  <Button
+                    type="submit"
+                    bg="#32d4a4"
+                    color="white"
+                    mr={3}
+                    onClick={onClose}
+                  >
+                    Continuar
+                  </Button>
+                  <Button onClick={onClose}>Cancelar</Button>
+                </ModalFooter>
+              </ModalContent>
+            </form>
+          </Modal>
+        )}
       </Formik>
     </>
   );
